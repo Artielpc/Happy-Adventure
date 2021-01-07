b8665f0a27e25b05165986fca45bfe6665cd83a3
Artielpc <apalomar@mbdanalytics.com>
2021-01-07 12:59:21 +0100

    Añadida estructura básica

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, render_template, redirect, url_for, session, flash, Markup, send_from_directory
+from flask_sqlalchemy import SQLAlchemy, inspect
+from flask_login import UserMixin, login_required, LoginManager, login_user, logout_user, current_user
+import base64
+import numpy as np
+import os
+import pandas as pd
+import random
+from werkzeug.utils import secure_filename
+import json
+import time
+import socket
+from datetime import datetime
+
+
+app = Flask(__name__)
+app.secret_key = '123456789'
+
+# diccionario para gestionar las valoraciones
+session_dict = {}
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/')
+def index():
+	print("ESTA ES UNA PRUEBA")
+	return render_template('index.html')	
+
+
+@app.route('/sentView', methods=['POST','GET'])
+def sentView():
+	print("OK")
+	return render_template('survey_completed.html')	
+
+
+if (__name__=="__main__"):
+	app.run(debug=True, threaded=True)
