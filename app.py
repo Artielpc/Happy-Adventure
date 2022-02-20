3811a2a3ffeb7173fe4a7f82a58c33030ef340ea
Artielpc <apalomar@mbdanalytics.com>
2022-02-20 20:54:25 +0100

    Añadidas funcionalidades de Probabilidad, limpieza y cambio de logo

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, request, render_template, url_for, session
-from flask_sqlalchemy import SQLAlchemy
-import numpy as np
-import os
-import pandas as pd
-import random
 import datetime
+import pickle as pk
 
+import numpy as np
+import pandas as pd
+from flask import Flask, render_template, request, session, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = '123456789'
@@ -19,13 +18,14 @@ db = SQLAlchemy(app)
 class Scores(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     hora = db.Column(db.DateTime, nullable=False)
-    edad = db.Column(db.Integer, nullable=True)
-    sexo = db.Column(db.String(6), nullable=False)
-    amigos = db.Column(db.Integer, nullable=False)
-    punto_compra = db.Column(db.String(1), nullable=True)
-    segmento = db.Column(db.String(1), nullable=False)
+    edad = db.Column(db.Float, nullable=False)
+    sexo = db.Column(db.String(10), nullable=False)
+    amigos = db.Column(db.Float, nullable=False)
+    punto_compra = db.Column(db.String(10), nullable=False)
+    segmento = db.Column(db.String(10), nullable=False)
     producto_A = db.Column(db.Float, nullable=False)
     producto_B = db.Column(db.Float, nullable=False)
+    probabilidad = db.Column(db.Float, nullable=False)
 
 db.create_all()
 
@@ -45,33 +45,66 @@ def index():
 	return render_template('index.html')	
 
 
-@app.route('/sentView', methods=['POST','GET'])
+@app.route('/sentView', methods=['POST'])
 def sentView():
-	print("Entro a sentView")
-	if request.method == 'POST':
-		print(request)
+    data = pd.DataFrame(request.form.to_dict(flat=False))
 
+    # Convertimos a numericas las variables necesarias
+    data = data.astype({'age': 'float', 'friends': 'float',
+                'prodA': 'float', 'prodB': 'float'})
 
+    data_original = data.copy()
+    data = data.rename(columns={'age': 'Edad', 'gender': 'Sexo', 'friends': 'Acompaniantes_amigos',
+        'prodA': 'Producto_A', 'prodB': 'Producto_B', 'punto-compra': 'Punto_Compra',
+        'segmento': 'Segmento_cliente'})
 
+    # Preparación de BBDD
+    data['Producto_total'] = data['Producto_A'] + data['Producto_B']
+    data['Producto_total_log'] = np.log(data['Producto_total'] + 1)
 
-		
-		hora = datetime.datetime.now()
-		registro = Scores(
-            hora = hora,
-			edad = request.form['age'], 
-            sexo = request.form['gender'],
-            amigos = request.form['friends'], 
-            punto_compra = request.form['punto-compra'],
-            segmento = request.form['segmento'],
-            producto_A = request.form['prodA'], 
-            producto_B = request.form['prodB'],  
-        )
+    data['Sexo_female'] = 0
+    data.loc[data['Sexo'] == 'female','Sexo_female'] = 1
 
-		db.session.add(registro)
-		db.session.commit()
+    data['Punto_Compra_S'] = 0
+    data.loc[data['Punto_Compra'] == 'S','Punto_Compra_S'] = 1
 
-	return render_template('survey_completed.html')	
+    data['Segmento_cliente_1'] = 0
+    data.loc[data['Segmento_cliente'] == '1','Segmento_cliente_1'] = 1
+    data['Segmento_cliente_2'] = 0
+    data.loc[data['Segmento_cliente'] == '2','Segmento_cliente_2'] = 1
+    data['Segmento_cliente_3'] = 0
+    data.loc[data['Segmento_cliente'] == '3','Segmento_cliente_3'] = 1
+
+    quitar = ['Sexo', 'Producto_A', 'Producto_B', 'Producto_total', 
+            'Punto_Compra', 'Segmento_cliente']
+    data = data.drop(quitar, axis=1)
+
+    prediction = modelo.predict(data)
+
+    data_original['probabilidad'] = prediction
+	
+    hora = datetime.datetime.now()
+
+    registro = Scores(
+        hora = hora,
+		edad = data_original['age'].values[0], 
+        sexo = data_original['gender'].values[0],
+        amigos = data_original['friends'].values[0], 
+        punto_compra = data_original['punto-compra'].values[0],
+        segmento = data_original['segmento'].values[0],
+        producto_A = data_original['prodA'].values[0], 
+        producto_B = data_original['prodB'].values[0],
+        probabilidad = data_original['probabilidad'].values[0] 
+    )
+
+    db.session.add(registro)
+    db.session.commit()
+
+    return render_template('survey_completed.html', probabilidad = round(data_original['probabilidad'].values[0] * 100))	
 
 
 if (__name__=="__main__"):
-	app.run(host='0.0.0.0', port=5000, debug=False, threaded=True)
+    # Código para leer el modelo entrenado
+    modelo = pk.load(open('modelo_rl.sav', 'rb'))
+
+    app.run(host='0.0.0.0', port=5000, debug=False, threaded=True)
